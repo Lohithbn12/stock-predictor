68636a5abc8b277bdb214ce26a9d20879955d01c
Lohithbn12 <lohithbn.s1814182@gmail.com>
2026-02-17 20:11:09 +0530

    Updated in stockpage

--- client/src/StockPage.jsx
+++ client/src/StockPage.jsx
@@ -6,7 +6,7 @@ import PredictionTablePage from "./PredictionTablePage";
 
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://stock-predictor-0zst.onrender.com";
 
 
 function StockPage() {
